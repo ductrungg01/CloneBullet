830b739866febdd78a88690c91f4cc7783c8b0f1
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-03-31 05:48:23 +0700

    Clone bullet when collide with boost

---
+++ CloneBullet/Assets/Scripts/Gameplay/Boost/Boost.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+    public GameObject rootObject;
+    public int boostValue;
+    public TextMeshProUGUI valueText;
+    public bool isUsed = false;
+
+    void Start()
+    {
+        ShowValue();
+    }
+
+    void ShowValue()
+    {
+        valueText.text = boostValue.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isUsed) return;
+
+        if (other.CompareTag("Bullet"))
+        {
+            isUsed = true;
+            CloneBullet(other.gameObject);
+        }
+        
+        
+    }
+
+    async UniTask CloneBullet(GameObject bullet)
+    {
+        Vector3 velocity = bullet.GetComponent<Rigidbody>().velocity;
+        
+        for (int i = boostValue; i > 0; i--)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(0.2f));
+
+            GameObject newBullet = PoolManager.Instance.bullet.OnTakeFromPool(
+                this.transform.position,
+                Quaternion.identity);
+
+            newBullet.GetComponent<Rigidbody>().velocity = velocity;
+
+            boostValue--;
+            ShowValue();
+        }
+        
+        Destroy(rootObject);
+    }
+}
